2bac98ea116ffec92de631626a26dd48d056bf00
Nguyễn Quang Đại <40832101+quangdaist123@users.noreply.github.com>
2023-01-03 17:31:49 +0700

    drop nan values

--- models/Multitask-Recommendation-Library/datasets/my_dataset.py
+++ models/Multitask-Recommendation-Library/datasets/my_dataset.py
@@ -5,6 +5,10 @@ import torch
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, dataset_path):
         data = pd.read_csv(dataset_path).to_numpy()[:, 1:]
+
+        # drop nan values
+        data = data.dropna()
+
         self.categorical_data = data[:, 4:54:5].astype(np.int)
         self.numerical_data = data[:, [i for i in range(2, data.shape[1]-3) if not ((4 <= i <= 49) and i%5 == 4)]].astype(np.float32)
         self.labels = data[:, -3:].astype(np.float32)
